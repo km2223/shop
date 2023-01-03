b8afbb42926003645bdfb074861f6fb85f27b3ca
km2223 <mhmoudalkma22@gmail.com>
2023-01-03 12:58:37 +0300

    create model

---
+++ lib/models/product.dart
@@ -0,0 +1,20 @@
+class Product{
+  final String title;
+  final String id;
+  final double price;
+  final String description;
+  final String imgeUrl;
+  
+  bool isfavorite;
+  Product({
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.id,
+    required this.imgeUrl,
+    required this.isfavorite
+  });
+
+
+
+}
