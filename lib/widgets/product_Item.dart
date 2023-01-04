1f42f14b0a8aca4c5b36569c0c42348a4cc5297a
km2223 <mhmoudalkma22@gmail.com>
2023-01-04 12:51:05 +0300

    favorite

--- lib/widgets/product_Item.dart
+++ lib/widgets/product_Item.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/provvider/product.dart';
+import 'package:shop/provvider/products.dart';
 import 'package:shop/widgets/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
+/*   final String id;
   final String title;
   final String imageUrl;
   ProductItem({
      required this.id,
      required this.imageUrl,
      required this.title
-  });
+  }); */
 
   @override
   Widget build(BuildContext context) {
+    final product=Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10 ),
       child:  GridTile( child:GestureDetector(
         onTap: () {
-          Navigator.of(context).pushNamed( ProductDetailScreen.routeName,arguments: id);
+          Navigator.of(context).pushNamed( ProductDetailScreen.routeName,arguments:product.id);
         },
-        child: Image.network(imageUrl,
+        child: Image.network(product.imageUrl,
         fit:BoxFit.cover,
         ),
       ) ,
       footer: GridTileBar(
-        title:Text(title,
+        title:Text(product.title,
         textAlign: TextAlign.center,),
         backgroundColor:Colors.black87,
-        leading:IconButton(icon:Icon(Icons.favorite) ,
+        leading:IconButton(icon:Icon(product.isfavorite? Icons.favorite:Icons.favorite_border) ,
         color: Theme.of(context).accentColor,
-         onPressed: () {  },),
+         onPressed: () { product.taggleFavoritestatus();  },),
          trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {  },
         color: Theme.of(context).accentColor,
          ),
